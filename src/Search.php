f07f068541dd8ec1873a422f1bff3e968ed125f4
Ícaro Luiz <icarolsl@gmail.com>
2025-06-28 19:56:19 -0300

    first commit

---
+++ src/Search.php
@@ -0,0 +1,12 @@
+<?php
+namespace Yoku\LocateCep;
+
+class Search{
+    private $url = "https://viacep.com.br/ws/";
+
+    public function getAddressFromZipCode($zipCode):array{
+        $zipCode = preg_replace('/[^0-9]/', '', $zipCode);
+        $get = file_get_contents($this->url . $zipCode . '/json/');
+        return (array) json_decode($get);
+    }
+}
